f44a4c5f83b5ad0c46e5f6887a77a082f916e082
Vlad-Orlov <orlov.vlad.serg@gmail.com>
2021-05-11 01:07:30 +0200

    cleanup

--- src/PhysicsList.cpp
+++ src/PhysicsList.cpp
@@ -4,7 +4,6 @@
 
 #include "PhysicsList.hh"
 
-// Our basic physics packages needed to do simple decay
 #include "G4DecayPhysics.hh"
 #include "G4RadioactiveDecayPhysics.hh"
 #include "G4EmStandardPhysics.hh"
@@ -12,33 +11,15 @@
 PhysicsList::PhysicsList()
   : G4VModularPhysicsList()
 {
-  //setVerboseLevel(1);
-
-  /*
-   * All we have to do is tell GEANT4 that we are using these lists
-   */
-  // Default physics
   RegisterPhysics(new G4DecayPhysics());
-
-  // Radioactive Decay
   RegisterPhysics(new G4RadioactiveDecayPhysics());
-
-  // E&M physics
   RegisterPhysics(new G4EmStandardPhysics());
 }
 
-PhysicsList::~PhysicsList()
-{}
 
-/*
- * Cuts are a threshold value at which a secondary will not be created. From the 
- * documentation they note that this is because some of the elctromagnetic processes
- * will create infrared divergence if this is not set. There is also a max threshold
- * energy set at 10 GeV and can be changed by running "/cuts/setMaxCutEnergy" in the
- * command line of the run.
- *
- * We will just keep the defaults
- */
+PhysicsList::~PhysicsList(){}
+
+
 void PhysicsList::SetCuts()
 {
   G4VUserPhysicsList::SetCuts();
